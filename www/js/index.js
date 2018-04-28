f23d1fb0584e96d563b11a517af49c8b445d6a80
matriiixu <oani1_16@uni.worc.ac.uk>
2018-04-28 12:46:31 +0100

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,27 @@
-$(document).on("pageshow", onPageShow);
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/about/',
+      url: 'about.html',
+    },
+  ],
+    
+  // ... other parameters
+});
 
-function onPageShow() {
-    console.log("page shown");
-}
+var mainView = app.views.create('.view-main');
+var swiper = app.swiper.create('.swiper-container', {
+    speed: 100
+
+});
