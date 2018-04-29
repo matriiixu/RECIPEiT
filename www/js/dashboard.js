8e8503b3908f0a069eeb9460d053ab3386f3dc9b
matriiixu <oani1_16@uni.worc.ac.uk>
2018-04-29 12:40:41 +0100

    Update 1.3

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -1,20 +1,7 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    var image = document.getElementById('image').addEventListener('click',outputImg);
-    var file = document.getElementById('file');
-    var output = document.getElementById('output');
-    function outputImg(){
-        output.innerHTML = file.value;
-        console.log(file.value);
-    }
-    navigator.camera.cleanup(onSuccess, onFail);
-
-    function onSuccess() {
-        console.log("Camera cleanup success.")
-    }
-
-    function onFail(message) {
-        alert('Failed because: ' + message);
-    }
-        console.log(navigator.camera);
+var image = document.getElementById('image').addEventListener('click',outputImg);
+var file = document.getElementById('file');
+var output = document.getElementById('output');
+function outputImg(){
+    output.innerHTML = file.value;
+    console.log(file.value);
 }
