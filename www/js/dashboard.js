cf4f77e126cd976b3399326314b9a53940088be3
matriiixu <oani1_16@uni.worc.ac.uk>
2018-04-30 10:39:54 +0100

    Update

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -1,21 +1,24 @@
 document.addEventListener("deviceready",onDeviceReady,false);
 var destinationType; //used sets what should be returned (image date OR file path to image for example)
-var image, file, output;
+var image, file, output, phonegapcamera;
 function onDeviceReady() {
     
-    
+    alert("onDeviceready");
 	destinationType=navigator.camera.DestinationType;
     image = document.getElementById('image').addEventListener('click',outputImg);
     file = document.getElementById('file');
     output = document.getElementById('output');
+    phonegapcamera = document.getElementById('phonegapcamera').addEventListener('click', capturePhoto);
 }
 
 function capturePhoto() {
+     alert("capturePhoto");
 	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
 	destinationType: destinationType.DATA_URL });
 }
 	
 function onPhotoDataSuccess(imageData) {
+    alert("onPhotoDataSuccess");
 	var image = document.getElementById('image');
 	image.style.display = 'block';
 	image.src = "data:image/jpeg;base64," + imageData;
