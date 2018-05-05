8d8ab90a501aac139fb530cd5445b058cb32406c
matriiixu <oani1_16@uni.worc.ac.uk>
2018-05-05 10:51:20 +0100

    Update 1.3.7
    This is the final Update

--- www/js/home.js
+++ www/js/home.js
@@ -1,4 +1,4 @@
-Backendless.initApp("38D2266B-EF1A-0CF3-FF65-A690213C4300","D3546A40-29FC-EAD4-FFDB-52225A01A900");
+//Backendless.initApp("38D2266B-EF1A-0CF3-FF65-A690213C4300","D3546A40-29FC-EAD4-FFDB-52225A01A900");
 /*var search = document.getElementById('search').addEventListener('click', searchBarSubmit);
 function searchBarSubmit(){
     console.log('submitted');
